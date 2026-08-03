573b19bdd811afb46ca071e39646e1154cbc0914
kumar123varun <38451072+kumar123varun@users.noreply.github.com>
2026-08-03 21:19:36 +0530

    updated

---
+++ calculator.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+add = num1 + num2
+
+print("The result of the addition is:", add)
+
+sub = num1 - num2
+
+print("The result of the subtraction is:", sub)
+
+mul = num1 * num2
+print("The result of the multiplication is:", mul)
+
+div = num1 / num2
+print("The result of the division is:", div)
+
