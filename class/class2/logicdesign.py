9053bd50fb130778f928663a5b3646708419e737
kumar123varun <38451072+kumar123varun@users.noreply.github.com>
2026-08-06 23:19:10 +0530

    updated

---
+++ class/class2/logicdesign.py
@@ -0,0 +1,7 @@
+class LogicDesign:
+    def logic_design(self, a, b):
+        print("AND operation:", a & b) 
+        print("OR operation:", a | b)
+        print("NOT operation:", ~a)
+        print("XOR operation:", a ^ b)
+ 
