178e5c0fa6079065b5c6ddb04fa9a1601037345e
kumar123varun <38451072+kumar123varun@users.noreply.github.com>
2026-08-14 19:28:50 +0530

    update

---
+++ Python/class/listtupleset.py
@@ -0,0 +1,26 @@
+num_with_dups = {1, 2, 3, 4, 5, 4, 3, 2, 1}
+print(num_with_dups)
+change_ds = tuple(num_with_dups)
+print(change_ds)
+
+# (.venv) PS D:\GenAI> & d:\GenAI\.venv\Scripts\python.exe d:/GenAI/Python/class/listtupleset.py
+# [1, 2, 3, 4, 5, 4, 3, 2, 1]
+# {1, 2, 3, 4, 5}
+# (.venv) PS D:\GenAI> & d:\GenAI\.venv\Scripts\python.exe d:/GenAI/Python/class/listtupleset.py
+# [1, 2, 3, 4, 5, 4, 3, 2, 1]
+# (1, 2, 3, 4, 5, 4, 3, 2, 1)
+# (.venv) PS D:\GenAI> & d:\GenAI\.venv\Scripts\python.exe d:/GenAI/Python/class/listtupleset.py
+# (1, 2, 3, 4, 5, 4, 3, 2, 1)
+# [1, 2, 3, 4, 5, 4, 3, 2, 1]
+# (.venv) PS D:\GenAI> & d:\GenAI\.venv\Scripts\python.exe d:/GenAI/Python/class/listtupleset.py
+# (1, 2, 3, 4, 5, 4, 3, 2, 1)
+# {1, 2, 3, 4, 5}
+# (.venv) PS D:\GenAI> & d:\GenAI\.venv\Scripts\python.exe d:/GenAI/Python/class/listtupleset.py
+# {1, 2, 3, 4, 5}
+# {1, 2, 3, 4, 5}
+# (.venv) PS D:\GenAI> & d:\GenAI\.venv\Scripts\python.exe d:/GenAI/Python/class/listtupleset.py
+# {1, 2, 3, 4, 5}
+# [1, 2, 3, 4, 5]
+# (.venv) PS D:\GenAI> & d:\GenAI\.venv\Scripts\python.exe d:/GenAI/Python/class/listtupleset.py
+# {1, 2, 3, 4, 5}
+# (1, 2, 3, 4, 5)
